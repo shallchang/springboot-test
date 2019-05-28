ddf493906c0f698309cc078060bf696b1092e7a1
Shawn Zhang <shawn.zhang@medopad.com>
2019-05-28 18:03:29 +0100

    return 400 when catch special exceptions

--- src/main/java/controller/RoboticCleanerController.java
+++ src/main/java/controller/RoboticCleanerController.java
@@ -4,11 +4,13 @@ import document.RoboticCleanerRequestDocument;
 import document.RoboticCleanerResponseDocument;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import service.CleanOilPatchService;
+import service.exception.RobotOutBoundaryException;
 
 import java.util.ArrayList;
 
@@ -20,7 +22,7 @@ public class RoboticCleanerController {
     CleanOilPatchService service;
 
     @RequestMapping("/")
-    public ResponseEntity<RoboticCleanerResponseDocument> clean(@RequestBody RoboticCleanerRequestDocument request) {
+    public ResponseEntity clean(@RequestBody RoboticCleanerRequestDocument request) {
         log.info("Received request:" + request.toString());
 
         ArrayList<Integer> areaSize = request.getAreaSize();
@@ -28,9 +30,16 @@ public class RoboticCleanerController {
         ArrayList<ArrayList<Integer>> oilPatches = request.getOilPatches();
         String navigationInstructions = request.getNavigationInstructions();
 
-        RoboticCleanerResponseDocument response = service.cleanPatch(areaSize, startingPosition, oilPatches, navigationInstructions);
-
-        return ResponseEntity.ok(response);
+        try {
+            RoboticCleanerResponseDocument response = service.cleanPatch(areaSize, startingPosition, oilPatches, navigationInstructions);
+            return ResponseEntity.ok(response);
+        }catch (RobotOutBoundaryException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }catch (Exception e){
+            return new ResponseEntity(HttpStatus.I_AM_A_TEAPOT);
+        }
     }
 
 }
